c6d3be1dfc476dbe77570bdc9684832d23d57dd7
yinjimmy <jimmy20250605@gmail.com>
2026-08-24 17:18:11 +0800

    fix: allow later assessment items in annual plan

--- backend/tests/test_v0003_startup_bootstrap.py
+++ backend/tests/test_v0003_startup_bootstrap.py
@@ -55,6 +55,8 @@ _EXPECTED_VERSIONS = (
     "0012_team_analytics_indexes",
     "0013_plan_item_growth_goal_nullable",
     "0014_evidence_archive_backfill",
+    "0015_plan_month_text",
+    "0016_plan_item_later_assessment",
 )
 
 
